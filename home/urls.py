7788bf0a58afe762024013bac53cf7f1e2755205
pavanchaudhar <pavanchaudhari035@gmail.com>
2026-07-18 22:10:09 +0530

    make new app name home

--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from home.views import home
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home),
-]
-# test commit
